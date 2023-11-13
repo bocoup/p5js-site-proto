d52fa3e739f4230b6207aca6c9951c143a37b23d
Stalgia Grigg <stalgia@bocoup.com>
2023-11-13 15:38:38 -0800

    Interactive build test

---
+++ src/components/InteractiveComponentTest/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const InteractiveComponentTest = () => {
+  const [count, setCount] = React.useState(0);
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+  return (
+    <div>
+      <h2>Interactive Component Test</h2>
+      <p>Count: {count}</p>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={increment}>Increment</button>
+    </div>
+  );
+};
+
+export default InteractiveComponentTest;
