87dc6a80c70375998ead055b52f20f60b836dea1
Stalgia Grigg <stalgia@bocoup.com>
2023-11-14 11:06:55 -0800

    Update build-reference script from astro branch

--- scripts/build-reference.js
+++ scripts/build-reference.js
@@ -7,20 +7,45 @@ import remarkMDX from "remark-mdx";
 let fileContextToModuleMap = new Map();
 let fileContextToSubmoduleMap = new Map();
 
+const modulePathTree = {};
+
 function getModulePath(doc) {
-  const prefix = `./pages/en/reference`;
-  const module = fileContextToModuleMap[doc.context.file];
+  let prefix = `./src/pages/en/reference`;
+  const module = fileContextToModuleMap[doc.context.file]?.toLowerCase();
+  const submodule = fileContextToSubmoduleMap[doc.context.file]?.toLowerCase();
+
+  // Check if module exists
   if (!module) {
     console.warn(
       `Could not find module for file ${doc.context.file} in fileContextToModuleMap`
     );
     return prefix;
   }
-  const submodule = fileContextToSubmoduleMap[doc.context.file];
-  if (!submodule) {
-    return `${prefix}/${module}`;
+
+  let path = `${prefix}/${module}`;
+
+  // Add submodule to path if it exists
+  if (submodule) {
+    path += `/${submodule}`;
   }
-  return `${prefix}/${module}/${submodule}/`;
+
+  const pathWithFile = `${path.replace("./src/pages", "")}/${doc.name}`;
+
+  // Create or update modulePathTree
+  if (!modulePathTree[module]) {
+    modulePathTree[module] = {};
+  }
+
+  if (submodule) {
+    if (!modulePathTree[module][submodule]) {
+      modulePathTree[module][submodule] = {};
+    }
+    modulePathTree[module][submodule][doc.name] = pathWithFile;
+  } else {
+    modulePathTree[module][doc.name] = pathWithFile;
+  }
+
+  return path;
 }
 
 function convertToMDX(doc) {
@@ -47,12 +72,65 @@ function convertToMDX(doc) {
     return;
   }
 
+  const transformedParams = doc.params
+    .map((param) => {
+      // Check if the necessary properties exist
+      if (!param.description || !param.description.children || !param.type) {
+        return null;
+      }
+
+      // Extract the description text
+      const descriptionText = param.description.children
+        .map((child) =>
+          child.children.map((textNode) => textNode.value).join("")
+        )
+        .join("");
+
+      return {
+        name: param.name,
+        description: descriptionText,
+        type: param.type.name ?? param.type.expression.name,
+      };
+    })
+    .filter((param) => param != null);
+
+  let descriptionText = "";
+
+  for (const child of doc.description?.children ?? []) {
+    for (const textNode of child.children) {
+      switch (textNode.type) {
+        case "inlineCode":
+          descriptionText += `\`${textNode.value}\``;
+          break;
+        case "link":
+          descriptionText += `[${textNode.children[0].value}](${textNode.url})`;
+          break;
+        case "strong":
+          descriptionText += `**${textNode.children[0].value}**`;
+          break;
+        case "emphasis":
+          descriptionText += `*${textNode.children[0].value}*`;
+          break;
+        case "paragraph":
+          descriptionText += `${textNode.children[0].value}`;
+          break;
+        case "text":
+        default:
+          descriptionText += textNode.value;
+          break;
+      }
+    }
+  }
+
   // Create the frontmatter string
   const frontmatter = matter.stringify("", {
+    layout: "@layouts/reference/SingleReferenceLayout.astro",
     title: doc.name,
     module,
     submodule,
-    examples: doc.examples.map((example) => example.description),
+    // This is currently a static value but might change
+    descriptionText,
+    params: transformedParams,
     // Add all properties as frontmatter, except for those that are objects
     // Likely needs to be organized more deliberately
     ...Object.entries(doc)
@@ -61,6 +139,7 @@ function convertToMDX(doc) {
           typeof value !== undefined && typeof value !== "object"
       )
       .reduce((acc, [key, value]) => ({ ...acc, [key]: value }), {}),
+    examples: doc.examples.map((example) => example.description),
   });
 
   // Combine all pieces of the doc into a single Markdown string
@@ -72,6 +151,40 @@ function convertToMDX(doc) {
   // Combine frontmatter and MDX content
   return `${frontmatter}\n${mdxContent.toString()}`;
 }
+
+const getIndexMdx = () => {
+  const frontmatter = matter.stringify("", {
+    title: "Reference",
+  });
+
+  let markdownContent = `# Reference\n`;
+
+  for (const module in modulePathTree) {
+    markdownContent += `## ${module}\n`;
+    const submodules = modulePathTree[module];
+
+    // Check if the module has submodules
+    if (Object.keys(submodules).length > 0 && typeof submodules === "object") {
+      for (const submodule in submodules) {
+        markdownContent += `### ${submodule}\n`;
+        for (const docName in submodules[submodule]) {
+          const path = submodules[submodule][docName];
+          markdownContent += `- [${docName}](${path})\n`;
+        }
+      }
+    } else {
+      // Handling the case where the module has no submodules
+      for (const docName in submodules) {
+        const path = submodules[docName];
+        markdownContent += `- [${docName}](${path})\n`;
+      }
+    }
+  }
+
+  const mdxContent = remark().use(remarkMDX).processSync(markdownContent);
+
+  return `${frontmatter}\n${mdxContent.toString()}`;
+};
 
 async function main() {
   console.log("Building reference docs...");
@@ -89,6 +202,10 @@ async function main() {
     });
     await fs.writeFile(`${savePath}/${doc.name}.mdx`, mdx.toString());
   }
+
+  /* Save reference home for navigation */
+  const indexMdx = getIndexMdx();
+  await fs.writeFile(`./src/pages/en/reference/index.mdx`, indexMdx.toString());
 }
 
 main();
